dc7d66c06ad1fe00ad7bd48dd0f0dffb4c3c01a3
yuu655 <y09768591@gmail.com>
2026-03-18 17:26:14 +0900

    changed

--- src/app/(main)/mentors/components/mentors.js
+++ src/app/(main)/mentors/components/mentors.js
@@ -131,7 +131,7 @@ export default function Mentors({ mentors }) {
             </p>
           </div>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8">
             {filteredMentors.map((mentor) => (
               <div key={mentor.id}>
                 <Mentor mentor={mentor} />
