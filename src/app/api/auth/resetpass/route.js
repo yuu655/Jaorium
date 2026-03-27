0de798521c26d9fb26286fecb6c36573274705fa
yuu655 <y09768591@gmail.com>
2026-03-27 13:22:31 +0900

    add a function to reset a password

--- src/app/api/auth/resetpass/route.js
+++ src/app/api/auth/resetpass/route.js
@@ -10,7 +10,7 @@ export async function GET(request) {
     const cookieStore = cookies()
     const supabase = createServerClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+      process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY,
       {
         cookies: {
           getAll() { return cookieStore.getAll() },
