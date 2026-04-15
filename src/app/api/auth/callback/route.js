752e2330478f7807318628593c005b942c116fdb
yuu655 <y09768591@gmail.com>
2026-04-15 20:03:35 +0900

    changed proxy

--- src/app/api/auth/callback/route.js
+++ src/app/api/auth/callback/route.js
@@ -27,7 +27,7 @@ export async function GET(request) {
         },
       }
     )
-    const { error } = await supabase.auth.exchangeCodeForSession(code)
+    const { data, error } = await supabase.auth.exchangeCodeForSession(code)
     if (error) {
       throw error
     }
