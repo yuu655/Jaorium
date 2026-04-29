6c29249d7fe939c415cb1d545311e78e08ffaf2d
yuu655 <y09768591@gmail.com>
2026-04-29 19:03:43 +0900

    a

--- src/app/(userPage)/dashboard/Interview/[roomName]/page.js
+++ src/app/(userPage)/dashboard/Interview/[roomName]/page.js
@@ -2,10 +2,18 @@ import InterviewWrapper from "@/components/dashboard/livekit-token/interviewWrap
 import { createClient } from "@/lib/supabase/server";
 
 export default async function InterviewPage({ params }) {
+  const { roomName } = await params;
   const supabase = await createClient();
   const { data: { user } } = await supabase.auth.getUser();
+  const { data: meeting } = await supabase
+    .from("meetings")
+    .select("*")
+    .eq("id", roomName)
+    .single();
   
-  const { roomName } = await params;
+
+  
+  
   return (
     <>
       <InterviewWrapper roomName={roomName} userName={user.id} />
